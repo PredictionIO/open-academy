940168ea9b44d3237bed7cda92476d95a0b43034
amin10 <manna@mit.edu>
2016-02-12 15:54:58 -0500

    fb data analysis

--- AminManna/fb_spark/src/main/scala/fbSpark/DataProcessing.scala
+++ AminManna/fb_spark/src/main/scala/fbSpark/DataProcessing.scala
@@ -6,10 +6,12 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
 import org.joda.time.DateTime
 
+
 object DataProcessing extends App {
 
   val sparkConf: SparkConf = Common.getSparkConf("DATA_PROCESSING")
   val sparkContext: SparkContext = new SparkContext(sparkConf)
+  sparkContext.setLogLevel("WARN")
   val sqlContext: SQLContext = new SQLContext(sparkContext)
 
   import sqlContext.implicits._
@@ -41,30 +43,93 @@ object DataProcessing extends App {
     .toDF
     .dropDuplicates(Seq("userId"))
 
-    case class UsersAds(
-      userId: String,
-      utmSource: String,
-      utmCampaign: String,
-      utmMedium: String,
-      utmTerm: String,
-      utmContent: String)
+  println("usersDF:")
+  usersDF.show
 
-    val usersAds: DataFrame = sparkContext
-      .textFile("/Users/amin/Desktop/users/fb//users_ads.csv", 750)
-      .map(line => {
-        val fields = line.split(",")
+  // case class UsersAds(
+  //   userId: String,
+  //   utmSource: String,
+  //   utmCampaign: String,
+  //   utmMedium: String,
+  //   utmTerm: String,
+  //   utmContent: String)
 
-        UsersAds(
-          userId = fields(0),
-          utmSource = fields(1),
-          utmCampaign = fields(2),
-          utmMedium = fields(3),
-          utmTerm = fields(4),
-          utmContent = fields(5))
-      })
-      .toDF
-      .dropDuplicates(Seq("userId"))
+  // val usersAds: DataFrame = sparkContext
+  //   .textFile("/Users/amin/Desktop/users/fb//users_ads.csv", 750)
+  //   .map(line => {
+  //     val fields = line.split(",")
 
-      usersDF.join(usersAds, "userId").show
+  //     UsersAds(
+  //       userId = fields(0),
+  //       utmSource = fields(1),
+  //       utmCampaign = fields(2),
+  //       utmMedium = fields(3),
+  //       utmTerm = fields(4),
+  //       utmContent = fields(5))
+  //   })
+  //   .toDF
+  //   .dropDuplicates(Seq("userId"))
+
+  // println("usersAds:")
+  // usersAds.show
+
+  // val usersWithAds: DataFrame = usersDF.join(usersAds, "userId")
+  
+  // println("usersWithAds:")
+  // usersWithAds.show
+
+  case class Conversion(
+    userId: String,
+    itemId: String,
+    price: String,
+    conversionTime: Double)
+
+  val conversionDF: DataFrame = sparkContext
+    .textFile("/Users/amin/Desktop/users/fb/conversions.csv", 750)
+    .map(line => {
+      val fields: Array[String] = line.split(",")
+      val numFields: Int = fields.size
+
+      val userId: String = fields(0)
+      val itemId: String = fields(1)
+      val price: String = fields(2)
+      val timestamp: String = fields(4)
+      val milliTime: Double = DateTime
+        .parse(timestamp)
+        .getMillis
+        .toDouble / 1000
+
+      Conversion(userId, itemId, price, milliTime)
+    })
+    .toDF
+
+  println("conversionDF:")
+  conversionDF.show
+
+  var user_conversionDF = usersDF.join(conversionDF, "userId")
+  
+  // user_conversionDF.show
+
+  user_conversionDF = user_conversionDF.withColumn("time_since_signup", user_conversionDF("conversionTime") - user_conversionDF("time")) 
+
+  println("user_conversionDF:")
+  user_conversionDF.show
+
+  user_conversionDF = user_conversionDF.filter("time_since_signup < 2592000")
+
+  println("filtered user_conversionDF:")
+  user_conversionDF.show
+
+  user_conversionDF = user_conversionDF
+    .groupBy($"userId").agg(Map("price" -> "sum" ))
+
+  println("grouped by user user_conversionDF:")
+  user_conversionDF.describe().show
+
+  println("over 5000")
+  user_conversionDF = user_conversionDF
+    .filter("sum(price) > 5000")
+
+  user_conversionDF.describe().show
 
 }
