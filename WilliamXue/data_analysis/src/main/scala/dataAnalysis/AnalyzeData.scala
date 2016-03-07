9543cc9442f5d1edb89984189d64da7f8046f158
William <wgxue@mit.edu>
2016-03-07 12:45:15 -0500

    Loaded data files to parquet for faster loading.
    Also made data path less explicit to not show the entire directory.

--- WilliamXue/data_analysis/src/main/scala/dataAnalysis/AnalyzeData.scala
+++ WilliamXue/data_analysis/src/main/scala/dataAnalysis/AnalyzeData.scala
@@ -7,21 +7,9 @@ import org.apache.spark.SparkConf
   */
 object AnalyzeData extends App {
   val sparkConf: SparkConf = Common.getSparkConf("DATA_ANALYZER")
-  val DATAPATH = "/Users/williamxue/Dropbox\\ \\(MIT\\)/PredictionIO/fb/"
+  val DATAPATH = "../../../data"
 
   val analyzer: DataAnalyzer = new DataAnalyzer(sparkConf, DATAPATH)
-  // analyzer.showCombinedTable()
-  // analyzer.showItemsTable()
+
   analyzer.showCombinedTable()
-  // analyzer.showUsersPurchasesTable()
-  // println(analyzer.countUsers)
-  // println(analyzer.countUsersOverPrice(5000.00))
-  println(analyzer.countUsersItemBoughtTimeLimit(30))
-  println(analyzer.countUsersItemBoughtAbovePrice(30, 5000.00))
-  // println(analyzer.findHighestPrice)
-  // println(analyzer.findLowestPrice)
-  // println(analyzer.findAveragePrice)
-  // println(analyzer.findAveragePriceBought)
-  // println(analyzer.findEarliestSignUpDate)
-  // println(analyzer.findLatestSignUpDate)
 }
